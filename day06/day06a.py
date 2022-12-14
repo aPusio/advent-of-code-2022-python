23ff24a8002309016850c9b6f7c14ce6075a9ea1
Adam Pusio <adam.pusio@allegro.pl>
2022-12-14 10:08:59 +0100

    Day 6

---
+++ day06/day06a.py
@@ -0,0 +1,20 @@
+import re
+
+inputFile = "./input.txt"
+exampleInputFile = "./input-example.txt"
+
+
+if __name__ == '__main__':
+    with open(inputFile, 'r') as file:
+        lines = file.readlines()
+
+    result = 0
+    line = lines[0]
+    window_size = 4
+    for i in range(len(line) - window_size + 1):
+        batch = line[i:i + window_size]
+        if len(set(batch)) == len(batch):
+            print(f'result: {i+window_size}')
+            break
+
+    print(result)
