f128221a4a0f46ff85ed727559bab8b12963af20
Adam Pusio <adam.pusio@allegro.pl>
2022-12-21 22:18:44 +0100

    Day 9 - Part I

--- day09/day09a.py
+++ day09/day09a.py
@@ -4,36 +4,68 @@ exampleInputFile = "./input-example.txt"
 
 class Position:
     def __init__(self, y, x) -> None:
-        y = y,
-        x = x
+        self.y = y
+        self.x = x
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __str__(self):
+        return f"y:{self.y}, x:{self.x}"
 
 
-def convert_to_vector(split_line):
+def to_traverse_points(split_line, start_point: Position):
     direction = split_line[0]
-    length = split_line[1]
+    length = int(split_line[1])
+    length += 1
     if direction == "U":
-        return {"y": length, "x": 0}
+        return [Position(i, start_point.x) for i in range(start_point.y+1, start_point.y + length)]
     elif direction == "D":
-        return {"y": -length, "x": 0}
+        return [Position(i, start_point.x) for i in range(start_point.y-1, start_point.y - length, -1)]
     elif direction == "L":
-        return {"y": 0, "x": -length}
+        return [Position(start_point.y, i) for i in range(start_point.x-1, start_point.x - length, -1)]
     elif direction == "R":
-        return {"y": 0, "x": length}
+        return [Position(start_point.y, i) for i in range(start_point.x+1, start_point.x + length)]
+
+
+def get_neighbours_and_itself(head: Position):
+    return [Position(head.y - 1, head.x - 1),
+            Position(head.y - 1, head.x),
+            Position(head.y - 1, head.x + 1),
+            Position(head.y + 1, head.x - 1),
+            Position(head.y + 1, head.x),
+            Position(head.y + 1, head.x + 1),
+            Position(head.y, head.x - 1),
+            Position(head.y, head.x + 1),
+            Position(head.y, head.x)]
 
 
 def main():
     with open(inputFile, 'r') as file:
         lines = file.readlines()
-    result = 0
     head = Position(0, 0)
     tail = Position(0, 0)
-    visited_positions = {}
+    visited_positions = {tail}
 
     for line in lines:
-        vector = convert_to_vector(line.split(" "))
-        # for
-
-    print(result)
+        print(f'command: {line}')
+        traverse_points = to_traverse_points(line.split(" "), head)
+        for traverse_point in traverse_points:
+            moved_head = Position(traverse_point.y, traverse_point.x)
+            print(f'head moved to: {moved_head}')
+            head_neighbours = get_neighbours_and_itself(moved_head)
+            if tail in head_neighbours:
+                print("Tail stays")
+            else:
+                tail = head
+                visited_positions.add(tail)
+                print(f'Tail moves {tail}')
+            head = moved_head
+        # print(traverse_points)
+    print(len(visited_positions))
 
 
 if __name__ == '__main__':
